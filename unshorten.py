7921f915849f099f75388a482b1897645f9fc35e
Yahya Torsh'kho <nagios84@gmail.com>
2018-11-30 16:17:14 +0300

    Update unshorten.py
    Cut browser opener

--- unshorten.py
+++ unshorten.py
@@ -1,7 +1,6 @@
 #!C:\Users\papa\AppData\Local\Programs\Python\Python37-32\python.exe
 import requests
 from clipboard import copy
-import webbrowser
 
 def get_url(short):
 	URL = 'https://linkunshorten.com/api/link?url='
@@ -14,7 +13,3 @@ short = input('Enter short URL:> ')
 addr = get_url(short)
 copy(addr)
 print('Your URL copied!')
-
-opera = 'C:\\Program Files\\Opera\\launcher.exe'
-webbrowser.register('opera', None, webbrowser.BackgroundBrowser(opera))
-webbrowser.get('opera').open_new_tab(addr)
